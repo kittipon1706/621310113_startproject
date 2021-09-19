cf8dcab5d3bd5fcc414573478ba69819ad56dd5c
LAPTOP-2FLV36AP\kitti <kittiponkong0604@gmail.com>
2021-09-19 18:30:22 +0700

    Final Edit

---
+++ ChatRoom_Server/Assets/Scripts/model/BaseModel.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class BaseModel
+{
+    public string ClassName { get; set; }
+    protected BaseModel(string className) => ClassName = className;
+}
